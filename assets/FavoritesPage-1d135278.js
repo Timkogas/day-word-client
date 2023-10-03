304c8e07679a01047f0fdb8c9b7267c62b3220ef
timkogas <timkogas@gmail.com>
2023-10-03 16:55:38 +0300

    Updates

---
+++ assets/FavoritesPage-1d135278.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-f1ec9568.js";const s=()=>e.jsx("div",{children:e.jsx("h1",{children:"Favorites Page"})});export{s as default};
---
+++ assets/WordsPage-908f3a4c.js
@@ -0,0 +1,1 @@
+import{u as e,a as o,r,g as c,j as a,W as d}from"./index-f1ec9568.js";const n=s=>{var t;return(t=s.word)==null?void 0:t.days},W="_WordsPage_1q89j_1",g={WordsPage:W},u=()=>{const s=e(),t=o(n);return r.useEffect(()=>{s(c())},[s]),a.jsx("div",{className:g.WordsPage,children:a.jsx(d,{days:t})})};export{u as default};
