e5e58f8e4c993f61a23a7ca3a4c312705f31b7b0
timkogas <timkogas@gmail.com>
2023-10-03 17:14:14 +0300

    Updates

--- assets/FavoritesPage-87c084bb.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-b1cd111c.js";const s=()=>e.jsx("div",{children:e.jsx("h1",{children:"Favorites Page"})});export{s as default};
---
+++ assets/FavoritesPage-b2a5c3ed.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-70d58db8.js";const s=()=>e.jsx("div",{children:e.jsx("h1",{children:"Favorites Page"})});export{s as default};
